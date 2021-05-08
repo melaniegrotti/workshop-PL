8fce12c4af839e6b26f8f78fd54ac2641923a34f
Elisabeth Kohlhofer <elisabeth.kohlhofer@stud.fh-campuswien.ac.at>
2021-05-08 11:56:39 +0200

    validate() method done?

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Bookmark.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Bookmark.java
@@ -1,5 +1,19 @@
 package pt.ipp.isep.dei.examples.tdd.basic.domain;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Bookmark {
+
+    public boolean validate(String stringForUrl){
+
+        try {
+            URL urlToTest = new URL(stringForUrl);
+            return true;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }
--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
@@ -2,34 +2,48 @@ package pt.ipp.isep.dei.examples.tdd.basic.domain;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class BookmarkTest {
-public void ensureBookmarkCanBeAdded(){
-    //Arrange
-    List<URL> listURL = new ArrayList<>();
-    try {
-        URL ausOpera = new URL("http", "opera.org.au", "/whats-on/brisbane/");
-    } catch (MalformedURLException e) {
-        e.printStackTrace();
+
+    // Integration Test?
+    /*String protocol = "http";
+        String host = "opera.org.au";
+        String file = "/whats-on/brisbane";
+        */
+
+    /*  @Test
+        public void ensureBookmarkCanBeAdded() {
+        //Arrange
+        List<URL> expectedURLList = new ArrayList<>();
+        try {
+            URL ausOpera = new URL("http", "opera.org.au", "/whats-on/brisbane/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        //Act
+        expectedURLList.add();
+
+        //Assert
+    } */
+
+    @Test
+    public void ensureURLIsValid() {
+
+        //Arrange
+        String stringUrl = "http://opera.org.au/whats-on/brisbane";
+
+        //Act
+        boolean result = new Bookmark().validate(stringUrl);
+
+        //Assert
+        assertTrue(result);
+
     }
-
-
-    //Act
-
-    //Assert
-    }
-
-
-    //public void ensureURIIsValid
-
-    //Arrange
-
-    //Act
-
-    //Assert
-
 
 
 }
