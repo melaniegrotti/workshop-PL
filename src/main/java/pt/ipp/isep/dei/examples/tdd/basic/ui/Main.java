1a41a2d147fa2e4d907df7814505e5d335f84478
Elisabeth Kohlhofer <elisabeth.kohlhofer@stud.fh-campuswien.ac.at>
2021-05-11 19:45:01 +0200

    ensureAddingATagToUrlWorks() tried in Main

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
@@ -6,6 +6,7 @@ import pt.ipp.isep.dei.examples.tdd.basic.domain.Calculator;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -15,6 +16,7 @@ public class Main {
         //System.out.println(calculator.sum(3, 4));
         Bookmark bookmark = new Bookmark();
         List<URL> urlList = new ArrayList<URL>();
+        HashMap<String, URL> hashMap = new HashMap<String, URL>();
 
         // VALIDATE URL
         String url1 = "https://www.britannica.com/animal/hamster";
@@ -41,5 +43,21 @@ public class Main {
         } catch (MalformedURLException e){
             System.out.println("Invalid URL");
         }
+
+        // ADD KEY + VALUE TO HASHMAP
+        String tag1 = "PetCare";
+        String tag2 = "Culture";
+        try {
+            URL urlForTag1 = new URL("https://www.britannica.com/animal/hamster");
+            URL urlForTag2 = new URL("http://opera.org.au/whats-on/brisbane");
+            bookmark.addTagToUrl(hashMap, urlForTag1, tag1);
+            bookmark.addTagToUrl(hashMap, urlForTag2, tag2);
+            System.out.println(hashMap);
+        }catch (MalformedURLException e){
+            System.out.println("Invalid Input");
+        }
+
+
+
     }
 }
